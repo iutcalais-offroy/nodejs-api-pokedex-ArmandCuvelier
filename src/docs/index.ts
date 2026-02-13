a986a725550d37bceb79bdb1433e5291fffd8c4b
Cuvelier Armand <armandcuvelier@gmail.com>
2026-02-13 13:52:14 +0100

    feat: test CI workflow

--- src/docs/index.ts
+++ src/docs/index.ts
@@ -1,18 +1,17 @@
 import YAML from 'yamljs'
 import path from 'path'
-import { fileURLToPath } from 'url'
 
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = path.dirname(__filename)
+const currentDir = process.cwd()
+const docsPath = path.join(currentDir, 'src', 'docs')
 
 // Charger la configuration principale
-const swaggerConfig = YAML.load(path.join(__dirname, 'swagger.config.yml'))
+const swaggerConfig = YAML.load(path.join(docsPath, 'swagger.config.yml'))
 
 // Charger les documentations des modules
-const authDoc = YAML.load(path.join(__dirname, 'auth.doc.yml'))
-const userDoc = YAML.load(path.join(__dirname, 'user.doc.yml'))
-const cardDoc = YAML.load(path.join(__dirname, 'card.doc.yml'))
-const deckDoc = YAML.load(path.join(__dirname, 'deck.doc.yml'))
+const authDoc = YAML.load(path.join(docsPath, 'auth.doc.yml'))
+const userDoc = YAML.load(path.join(docsPath, 'user.doc.yml'))
+const cardDoc = YAML.load(path.join(docsPath, 'card.doc.yml'))
+const deckDoc = YAML.load(path.join(docsPath, 'deck.doc.yml'))
 
 // Fusionner tous les paths
 export const swaggerDocument = {
